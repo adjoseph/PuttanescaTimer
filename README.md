a71af68aa0cad0db618008c45c8e8c0b681a0f66
adjoseph <aarondylanjoseph@gmail.com>
2017-01-22 15:10:40 -0800

    included angular material dependencies for About modal. Included angular-sanitize for modal html contents

--- README.md
+++ README.md
@@ -14,7 +14,6 @@ Bugs:
 - time initially displays with only one 0 in the minutes place
   - Bug fixed, but in a haphazard way: if seconds == 0, it displays as the string "00"
     - there must be a better way, but it doesn't matter for right now
-- upon adding custom time, new custom time should be default in dropdown menu
 - ding only happens after the alert pops up, should happen before
 - persist custom times through reloads and page changes
   - refactor code so that variables are global?
--- app/app.js
+++ app/app.js
@@ -5,10 +5,34 @@ angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
-  'myApp.version'
+  'myApp.version',
+  'ngMaterial',
+  'ngSanitize'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
 
   $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+}])
+
+.controller('AppCtrl', function($scope, $mdDialog) {
+	$scope.status = '  ';
+	$scope.customFullscreen = false;
+
+
+	$scope.showAbout = function(ev) {
+	    // Appending dialog to document.body to cover sidenav in docs app
+	    // Modal dialogs should fully cover application
+	    // to prevent interaction outside of dialog
+	    $mdDialog.show(
+	      $mdDialog.alert()
+	        .parent(angular.element(document.querySelector('#popupContainer')))
+	        .clickOutsideToClose(true)
+	        .title('About')
+	        .htmlContent('The Pomodoro Technique is a time management method.' + '<br>' +  'The goal of the technique is to facilitate iterative development by breaking work into intervals')
+	        .ariaLabel('Alert Dialog Demo')
+	        .ok('Got it!')
+	        .targetEvent(ev)
+	    )
+	  }
+})
